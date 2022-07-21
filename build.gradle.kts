8f52eac0a41d577a0d2c6343e13640ae29cc660d
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-21 18:10:04 +0400

    build: Add telegrambots-abilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.telegram:telegrambots:6.1.0")
+    implementation("org.telegram:telegrambots-abilities:6.1.0")
     implementation("org.telegram:telegrambots-spring-boot-starter:6.1.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
