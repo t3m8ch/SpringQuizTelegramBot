407fea77c49cb870a6adff1245c1a0c29f4823e1
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-22 13:36:08 +0400

    feat: Show the count of pages in the quiz paginator

--- src/main/kotlin/io/github/t3m8ch/quizbot/services/QuizService.kt
+++ src/main/kotlin/io/github/t3m8ch/quizbot/services/QuizService.kt
@@ -2,8 +2,9 @@ package io.github.t3m8ch.quizbot.services
 
 import io.github.t3m8ch.quizbot.models.QuizModel
 import org.bson.types.ObjectId
+import org.springframework.data.domain.Page
 
 interface QuizService {
-    fun getAll(pageIndex: Int = 0, pageSize: Int = 6): List<QuizModel>
+    fun getAll(pageIndex: Int = 0, pageSize: Int = 6): Page<QuizModel>
     fun getById(id: ObjectId): QuizModel
 }
--- src/main/kotlin/io/github/t3m8ch/quizbot/services/impl/QuizServiceImpl.kt
+++ src/main/kotlin/io/github/t3m8ch/quizbot/services/impl/QuizServiceImpl.kt
@@ -4,6 +4,7 @@ import io.github.t3m8ch.quizbot.models.QuizModel
 import io.github.t3m8ch.quizbot.repositories.QuizRepository
 import io.github.t3m8ch.quizbot.services.QuizService
 import org.bson.types.ObjectId
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.data.repository.findByIdOrNull
@@ -11,14 +12,14 @@ import org.springframework.stereotype.Service
 
 @Service
 class QuizServiceImpl(private val quizRepository: QuizRepository) : QuizService {
-    override fun getAll(pageIndex: Int, pageSize: Int): List<QuizModel> {
+    override fun getAll(pageIndex: Int, pageSize: Int): Page<QuizModel> {
         return quizRepository.findAll(
             PageRequest.of(
                 pageIndex,
                 pageSize,
                 Sort.by(Sort.Direction.DESC, "createdAt")
             )
-        ).toList()
+        )
     }
 
     // TODO: Change to custom exception
